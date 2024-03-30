f7081de0068eeba6d770967f90218a83b3683e21
Ranjith kumar <kranjith1433@gmail.com>
2024-03-30 16:52:15 +0530

    initial chatbot practice

---
+++ functionality.js
@@ -0,0 +1,11 @@
+const chatbotBtn= document.getElementById('chatbot-btn').addEventListener('click'{
+    console.log("Hi THis is chery how can i help you")
+    console.log("job")
+    console.log("job")
+    console.log("job")
+    console.log("job")
+    console.log("job")
+
+
+
+})
